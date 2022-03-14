a6b80e06d63684ac80e35938db6c024749f2b16b
fl0wer7777 <3057142482@qq.com>
2022-03-14 10:53:17 +0800

    刘
    外包小队后端

---
+++ src/main/java/edu/hfut/springboot_wbxd/SpringBootWbxdApplication.java
@@ -0,0 +1,13 @@
+package edu.hfut.springboot_wbxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWbxdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWbxdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hfut/springboot_wbxd/SpringBootWbxdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hfut.springboot_wbxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWbxdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
