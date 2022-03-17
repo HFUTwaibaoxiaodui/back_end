2f6154f357b1b067e7d0a5fc75cbb5a91dab55ec
autohe <1518585346@qq.com>
2022-03-17 21:38:21 +0800

    增加了前端string转后端date的方法，加入DateTimeFormat注解

--- src/main/java/edu/hfut/back_end/Entity/OperationLog.java
+++ src/main/java/edu/hfut/back_end/Entity/OperationLog.java
@@ -2,6 +2,7 @@ package edu.hfut.back_end.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigInteger;
 import java.util.Date;
@@ -17,6 +18,7 @@ public class OperationLog {
 
     String description;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     Date operationTime;
 
