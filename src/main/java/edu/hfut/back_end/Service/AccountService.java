e1fb3ccc895fb1ba5fcfd949a8afbb2ec1e153a0
autohe <1518585346@qq.com>
2022-03-20 23:53:53 +0800

    调整了接口

--- src/main/java/edu/hfut/back_end/Service/AccountService.java
+++ src/main/java/edu/hfut/back_end/Service/AccountService.java
@@ -40,6 +40,6 @@ public class AccountService {
     public boolean accountNameIsExist(String accountName){return accountMapper.accountNameIsExist(accountName);}
     public List<Account> selectAllInformation(){return accountMapper.selectAllInformation();}
     public void updateInformation(Account account){accountMapper.updateInformation(account);}
-    public List<Account> selectInformationById(BigInteger accountId){return accountMapper.selectInformationById(accountId);}
+    public Account selectInformationById(BigInteger accountId){return accountMapper.selectInformationById(accountId);}
     public BigInteger selectIdByAccountName(String accountName){return accountMapper.selectIdByAccountName(accountName);}
 }
