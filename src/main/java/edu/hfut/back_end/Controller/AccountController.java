c9e72d7db9ac00e332da0d51aeab5bff819df28c
吴涵禹 <1621076872@qq.com>
2022-03-23 11:37:16 +0800

    去掉多余函数

--- src/main/java/edu/hfut/back_end/Controller/AccountController.java
+++ src/main/java/edu/hfut/back_end/Controller/AccountController.java
@@ -92,11 +92,7 @@ public class AccountController {
         account.setCurrentTime(new Date());
         accountService.updateInformation(account);
     }
-
-    @RequestMapping(value = "/findAccountNameByAccountId", method = RequestMethod.GET)
-    @ApiOperation(value = "通过accountId查询信息", notes = "通过accountId查询信息")
-    public Account findContentByAccountId(BigInteger accountId) {
-        return accountService.findContentByAccountId(accountId);
+    
     @RequestMapping(value = "/selectAllInformation",method = RequestMethod.GET)
     @ApiOperation(value = "查找所有用户的信息")
     List<Account> selectAllInformation(){
